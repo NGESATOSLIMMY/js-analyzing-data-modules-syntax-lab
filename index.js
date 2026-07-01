debe95ce41a49b3af2a9a9d9a8369ab0bde83e26
gobetoslimmy <emmanuelngesa0@gmail.com>
2026-07-01 16:07:43 +0300

    Refactor combineUsers function and update package name in package-lock.json

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
+// Import the datejs library
+require("datejs"
+);
+
+// Create a function to combine users
+function combineUsers(...args) {
+ // Create an object that will hold the combined users
+  const combinedObject = {
+    users: [], //Create array to hold users and initialize with empty array
+  };
 
 
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
+  //  Loop through args(argument) one array at a time
+  for (const array of args) {
+    // Merge the users from current array into users
+    combinedObject.users = [
+      ...combinedObject.users, //Spread/Keep the exisitng usernames
+      ...array, //Add usernames from the current array
+    ];
+  }
+  // Add today's date in the format m/d/yyyy
+  combinedObject.merge_date = new Date().toString("M/d/yyyy");
+
+  // Return the combined object
+  return combinedObject;
+}
