0155ba5e5a067ba295f330abc0417c80ef43d85f
gobetoslimmy <emmanuelngesa0@gmail.com>
2026-07-08 11:52:49 +0300

    Refactor module exports to ensure combineUsers is properly exported

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ function combineUsers(...args) {
   combinedObject.merge_date = new Date().toString("M/d/yyyy");
   return combinedObject;
 }
+
+module.exports = {
+  ...(typeof combineUsers !== "undefined" && { combineUsers }),
+};
